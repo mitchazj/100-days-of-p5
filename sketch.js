e2caaaf44e1f9e63c6ffb73efd9da79bba5993ac
Mitchell Johnson <mitchellazj@gmail.com>
2021-06-24 17:44:33 +1000

    Kinda maybe snakes?

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef, no-unused-vars */
 
-const DAY = 3; // change the day being drawn here
+const DAY = 4; // change the day being drawn here
 const PADDING = 0;
 const FRAMERATE = 60;
 
@@ -325,6 +325,174 @@ window.day3 = {
     // Place snake in centre
     this.snake.body[0].x = floor(this.grid.res_x / 2);
     this.snake.body[0].y = floor(this.grid.res_y / 2);
+  },
+
+  draw: function (p5) {
+    // Clear background
+    background(13, 15, 19);
+
+    // Put drawings here
+    for (let x = 0; x < this.grid.res_x; ++x) {
+      for (let y = 0; y < this.grid.res_y; ++y) {
+        noStroke();
+        fill(31, 134, 181, this.grid.data[x][y]);
+        rect(this.locateX(x, y), this.locateY(x, y), this.grid.w, this.grid.w);
+      }
+    }
+
+    // Update the snake
+    this.moveSnake(p5.deltaTime);
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// DAY 4
+////////////////////////////////////////////////////////////////////////////////////////////
+
+window.day4 = {
+  // Data
+  SET_COLOR: 150,
+  UNSET_COLOR: 50,
+  grid: { res_x: 10, res_y: 0, w: 0, data: [] },
+  snake: {
+    body: [{ x: 8, y: 1, direction: 1, length: 8 }],
+    state: 0
+  },
+  // 0 = up, 1 = right, 2 = down, 3 = left
+
+  // Custom methods
+  getDisplacedX: function (joint, distance = 1) {
+    return joint.direction === 1
+      ? joint.x + distance
+      : joint.direction === 3
+      ? joint.x - distance
+      : joint.x;
+  },
+
+  getDisplacedY: function (joint, distance = 1) {
+    return joint.direction === 0
+      ? joint.y - distance
+      : joint.direction === 2
+      ? joint.y + distance
+      : joint.y;
+  },
+
+  jointSnake: function (x, y, direction) {
+    let new_head = { x, y, direction, length: 0 };
+    this.snake.body.unshift(new_head);
+    return this.snake.body[0];
+  },
+
+  setColorAlongJoint: function (joint, color, l, a = -1) {
+    // Locate the previous point by reverse displacement
+    let x = this.getDisplacedX(joint, a * l);
+    let y = this.getDisplacedY(joint, a * l);
+    // Unset it gracefully
+    if (x >= 0 && x < this.grid.res_x && y >= 0 && y < this.grid.res_y) {
+      this.grid.data[x][y] = color;
+    }
+  },
+
+  moveSnake: function (delta) {
+    this.snake.state += 0.0002 * delta;
+    let lead = this.snake.body[0]; // Get the lead joint
+
+    // Are we moving this step?
+    if (this.snake.state > 1) {
+      // Have we hit an edge with the leading joint?
+      if (lead.x === this.grid.res_x - 1 && lead.direction === 1) {
+        // lead.direction = 2;
+        lead = this.jointSnake(lead.x, lead.y, 2);
+      }
+      if (lead.y === this.grid.res_y - 1 && lead.direction === 2) {
+        // lead.direction = 3;
+        lead = this.jointSnake(lead.x, lead.y, 3);
+      }
+      if (lead.x === 0 && lead.direction === 3) {
+        // lead.direction = 0;
+        lead = this.jointSnake(lead.x, lead.y, 0);
+      }
+      if (lead.y === 0 && lead.direction === 0) {
+        // lead.direction = 1;
+        lead = this.jointSnake(lead.x, lead.y, 1);
+      }
+
+      // Move the lead forwards
+      lead.x = this.getDisplacedX(lead);
+      lead.y = this.getDisplacedY(lead);
+
+      // Reset state counter
+      this.snake.state = 0;
+
+      // For each joint in the snakes body
+      for (let l = 0; l < this.snake.body.length; ++l) {
+        // Get the joint
+        let joint = this.snake.body[l];
+
+        // Is this the last joint?
+        if (l === this.snake.body.length - 1) {
+          --joint.length; // Decrement the end joint
+          ++lead.length; // Increment the lead joint
+          console.log(`Trimmed to: ${joint.length}`);
+        }
+
+        // Foreach square along the joint, set the color
+        for (let c = 0; c < joint.length; ++c) {
+          this.setColorAlongJoint(joint, this.SET_COLOR, c);
+        }
+
+        // Is this the last joint?
+        if (l === this.snake.body.length - 1) {
+          // Unset the last color
+          this.setColorAlongJoint(joint, this.UNSET_COLOR, joint.length);
+
+          if (joint.length === 0) {
+            // Yeet it away if the joint no longer has length
+            console.log(`>> Removed at: ${joint.length}`);
+            let dropped = this.snake.body.pop();
+            // Clean up the square left behind because we'll miss it next time.
+            this.setColorAlongJoint(dropped, this.UNSET_COLOR, 1);
+            // fill(131, 134, 181, 0.5);
+            // rect(this.locateX(x, y), this.locateY(x, y), this.grid.w, this.grid.w);
+          }
+        }
+      }
+    }
+  },
+
+  // This is really bad but I hacked it together super quickly and it
+  // mostly does the job
+  locateX: function (x, y) {
+    let gap =
+      (height() - this.grid.res_y * this.grid.w) / (this.grid.res_y + 1);
+    let start_x =
+      width() / 2 -
+      (this.grid.res_x * this.grid.w + gap * (this.grid.res_x - 1)) / 2;
+    return start_x + x * (this.grid.w + gap);
+  },
+
+  locateY: function (x, y) {
+    let gap =
+      (height() - this.grid.res_y * this.grid.w) / (this.grid.res_y + 1);
+    let start_y =
+      height() / 2 -
+      (this.grid.res_y * this.grid.w + gap * (this.grid.res_y - 1)) / 2;
+    return start_y + y * (this.grid.w + gap);
+  },
+
+  // Core drawing
+  setup: function () {
+    this.grid.w = floor(width() / (this.grid.res_x + 1));
+    this.grid.res_y = floor((this.grid.res_x + 1) * (height() / width()));
+    console.log(this.grid);
+    for (let x = 0; x < this.grid.res_x; ++x) {
+      this.grid.data.push(
+        [...new Array(this.grid.res_y)].map((x) => this.UNSET_COLOR)
+      );
+    }
+    // Place snake in centre
+    // this.snake.body[0].x = floor(this.grid.res_x / 2);
+    // this.snake.body[0].y = floor(this.grid.res_y / 2);
   },
 
   draw: function (p5) {
